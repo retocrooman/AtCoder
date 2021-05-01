8efd174e93ec877c267a8f28dc2338661ecf820f
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-05-01 23:01:08 +0900

    A UFO襲来

--- 2021_5_1_1.cpp
+++ 2021_5_1_1.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 
 int main(){
-    int score;
-    int N, L, K;
-    cin >> N >> L >> K;
-    vector<int> vec(N);
-    for(int i=0; i<N; i++) {
-        cin >> vec.at(i);
+    string str;
+    int cnt=0;
+    cin >> str;
+    for (int i; i<9; i++) {
+        if(str.substr(i,4) == "ZONe") {
+            cnt++;
+        }
     }
-    for(int i=1; i<N; i++) {
-        vec.at(i) -= vec.at(i-1);
-    }
-    vec.push_back(L-vec.at(N-1));
-    sort(vec.rbegin(), vec.rend());
-    for(int i=0; i<N-K; i++) {
-        vec.pop_back();
-    }
-    score = vec.at(vec.size()-1) + vec.at(vec.size()-2);
-    cout << score << endl;
+    cout << cnt << endl;
 } 
