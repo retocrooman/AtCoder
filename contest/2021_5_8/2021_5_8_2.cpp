ba0409018558ae3bf3f157905a538e611780ee4b
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-05-09 00:31:28 +0900

    A Century

---
+++ contest/2021_5_8/2021_5_8_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i=0; i<(int)(n); i++)
+
+int main() {
+    int N;
+    cin >> N;
+    N += 99;
+    N /= 100;
+    cout << N << endl;
+}
