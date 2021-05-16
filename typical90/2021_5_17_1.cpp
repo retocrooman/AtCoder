787b3620f769d18b1a4b6b0551589167adba4c7c
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-05-17 01:29:31 +0900

    020 - Log Inequality（★3）

---
+++ typical90/2021_5_17_1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i=0; i<(int)(n); i++)
+#define REP(i, n) for (int i=1; i<=(int)(n); i++)
+#define FOR(i, n, m) for (int i=(n); i<=(int)(m); i++)
+#define ll long long
+const ll INF=1LL<<60;
+const double pi=3.1415926535;
+
+int main() {
+    ll a,b,c;
+    cin >> a >> b >> c;
+    if (a < powl(c,b)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
