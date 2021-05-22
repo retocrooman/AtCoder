dca1acf919b1a0c587bc4d5ec5e18244f078f338
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-05-23 01:51:30 +0900

    A - Three Dice

---
+++ contest/2021_5_22_2/2021_5_22_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i=0; i<(int)(n); i++)
+#define REP(i, n) for (int i=1; i<=(int)(n); i++)
+#define FOR(i, n, m) for (int i=(n); i<=(int)(m); i++)
+#define ll long long
+const ll INF=1LL<<60;
+const double pi=3.1415926535;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    int ans = 21 - a - b -c;
+    cout << ans << endl;
+}
