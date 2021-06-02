79673a25b681525ed5c24e570bc3aab6a16f5c64
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-06-03 00:53:15 +0900

    C - Dice and Coin

---
+++ AtCoder problems/2019_5_19/2021_6_2_3.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i=0; i<(int)(n); i++)
+#define rev_rep(i, n) for (int i=(n); i > 0; i--)
+#define REP(i, n) for (int i=1; i<=(int)(n); i++)
+#define FOR(i, n, m) for (int i=(n); i<=(int)(m); i++)
+#define rev_FOR(i, n, m) for (int i=(n); i<=(int)(m); i--)
+#define ll long long
+#define vc vector<int>
+#define vc2 vector<vector<int>>
+#define vc3 vector<vector<vector<int>>>
+#define vcl vector<ll>
+#define vc2l vector<vector<ll>>
+#define vc3l vector<vector<vector<ll>>>
+#define vcd vector<double>
+#define vc2d vector<vector<double>>
+#define vc3d vector<vector<vector<double>>>
+#define ALL(vec) vec.begin(),vec.end()
+#define SIZE(vec) vec.size()
+#define pb push_back
+const ll INF=1LL<<60;
+const ll mod = 1000000007;
+const double pi=3.1415926535;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<class T>bool ch_max(T &a, const T &b) {if (a<b) {a = b; return 1; } return 0;}
+template<class T>bool ch_min(T &a, const T &b) {if (b<a) {a = b; return 1; } return 0;}
+
+int main() {
+    int N , K;
+    cin >> N >> K;
+
+    vcd ans(N);
+    REP(i, N) {
+        int sum = i;
+        int cnt = 0;
+        while (sum < K) {
+            sum *= 2;
+            cnt++;
+        }
+        ll a = 1;
+        rep(j, cnt) a *= 2;
+        ans.at(i-1) = ((double)1/N/a);
+    }
+
+    double answer = 0;
+    rep(i, N) answer += ans.at(i);
+    cout << fixed << setprecision(10) << answer << endl;
+}
