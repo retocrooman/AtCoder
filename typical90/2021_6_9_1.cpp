97c9bfbd1e3f3d2312c49d2345165bb784b96367
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-06-08 16:46:02 +0900

    061 - Deck（★2）

---
+++ typical90/2021_6_9_1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i=0; i<(int)(n); i++)
+#define rev_rep(i, n) for (int i=(n); i > 0; i--)
+#define REP(i, n) for (int i=1; i<=(int)(n); i++)
+#define FOR(i, n, m) for (int i=(n); i<=(int)(m); i++)
+#define rev_FOR(i, n, m) for (int i=(n); i<=(int)(m); i--)
+#define ll long long
+#define vc vector<int>
+#define vc2 vector<vector<int>>
+#define vc3 vector<vector<vector<int>>>
+#define vcl vector<ll>
+#define vc2l vector<vector<ll>>
+#define vc3l vector<vector<vector<ll>>>
+#define vcd vector<double>
+#define vc2d vector<vector<double>>
+#define vc3d vector<vector<vector<double>>>
+#define vcs vector<string>
+#define vc2s vector<vector<string>>
+#define vc3s vector<vector<vector<string>>>
+#define ALL(vec) vec.begin(),vec.end()
+#define SIZE(vec) vec.size()
+#define pb push_back
+#define greater greater<>()
+const ll INF=1LL<<60;
+const ll mod = 1000000007;
+const double pi=3.1415926535;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<class T>bool ch_max(T &a, const T &b) {if (a<b) {a = b; return 1; } return 0;}
+template<class T>bool ch_min(T &a, const T &b) {if (b<a) {a = b; return 1; } return 0;}
+
+int main() {
+    int Q;
+    cin >> Q;
+    deque<ll> deq;
+    rep(i, Q) {
+        ll t, x;
+        cin >> t >> x;
+        if (t == 1) {
+            deq.push_front(x);
+        }
+        else if (t == 2) {
+            deq.push_back(x);
+        }
+        else {
+            cout << deq.at(x-1) << endl;
+        }
+    }
+}
