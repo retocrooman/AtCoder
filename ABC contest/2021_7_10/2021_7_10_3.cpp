43847e900a58498c25a182cc23c48f98394ed2e8
retocrooman <83130624+retocrooman@users.noreply.github.com>
2021-07-11 08:39:52 +0900

    C - Not Equal

---
+++ ABC contest/2021_7_10/2021_7_10_3.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (ll i=0; i<(ll)(n); i++)
+#define rev_rep(i, n) for (ll i=(n); i > 0; i--)
+#define REP(i, n) for (ll i=1; i<=(ll)(n); i++)
+#define FOR(i, n, m) for (ll i=(n); i<=(ll)(m); i++)
+#define rev_FOR(i, n, m) for (ll i=(n); i>=(ll)(m); i--)
+#define ll long long
+#define ull unsigned long long
+#define vc vector<ll>
+#define vc2 vector<vector<ll>>
+#define vc3 vector<vector<vector<ll>>>
+#define vcd vector<double>
+#define vc2d vector<vector<double>>
+#define vc3d vector<vector<vector<double>>>
+#define vcs vector<string>
+#define vc2s vector<vector<string>>
+#define vc3s vector<vector<vector<string>>>
+#define ALL(vec) vec.begin(),vec.end()
+const ll INF=1LL<<60;
+const ll mod = 1000000007;
+const double pi=3.1415926535;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<class T>bool ch_max(T &a, const T &b) {if (a<b) {a = b; return 1; } return 0;}
+template<class T>bool ch_min(T &a, const T &b) {if (b<a) {a = b; return 1; } return 0;}
+
+int main() {
+    ll N;
+    cin >> N;
+    vc C(N);
+    rep(i, N) cin >> C.at(i);
+    sort(ALL(C));
+    ll left = C.at(0);
+    ll ans = 1;
+    rep(i, N) {
+        if(i != 0) left += C.at(i) - C.at(i-1) - 1;
+        if(left < 0) {
+            cout << 0 << endl;
+            return 0;
+        }
+        ans *= left%mod;
+        ans %= mod;
+    }
+    cout << ans << endl;
+}
